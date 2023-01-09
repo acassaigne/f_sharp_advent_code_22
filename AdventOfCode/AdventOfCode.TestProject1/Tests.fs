b38957619f1b8387aaa27eed88d202f1d7d25511
guillaume <guillaume.saintetienne@marvin-bot.com>
2023-01-09 18:42:42 +0100

    test: add FsUnit

--- AdventOfCode/AdventOfCode.TestProject1/Tests.fs
+++ AdventOfCode/AdventOfCode.TestProject1/Tests.fs
@@ -2,7 +2,12 @@ module Tests
 
 open System
 open Xunit
+open FsUnit.Xunit
+
 
 [<Fact>]
 let ``My test`` () =
     Assert.True(true)
+    
+[<Fact>]
+let ``Expected minutes in oven`` () = 42 |> should equal 42
