c9d83335344dcc92cdde5a39d6eaed306bd7bd0a
acassaigne <anthony.cassaigne@gmail.com>
2023-01-09 18:58:15 +0100

    test; first elves

--- AdventOfCode/AdventOfCode.TestProject1/Tests.fs
+++ AdventOfCode/AdventOfCode.TestProject1/Tests.fs
@@ -4,10 +4,8 @@ open System
 open Xunit
 open FsUnit.Xunit
 
+let CountElves (calories: string list) : int =
+    0
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
-    
-[<Fact>]
-let ``Expected minutes in oven`` () = 42 |> should equal 42
+let ``count elves`` () = CountElves [] |> should equal 0 
