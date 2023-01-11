b7c93da4c1607bb53381e257c3e59a9f8ea9a075
acassaigne <anthony.cassaigne@gmail.com>
2023-01-11 19:46:31 +0100

    feat: CountElves done

--- AdventOfCode/AdventOfCode.FunctionalDomain/DayOne.fs
+++ AdventOfCode/AdventOfCode.FunctionalDomain/DayOne.fs
@@ -1,5 +1,13 @@
 ﻿namespace FunctionalDomain
 
+open System.Text.RegularExpressions
+
+
 module DayOne =
-let CountElves (calories: string list) : int =
-    calories.Length
+let CountElves (calories: string list) : int = 
+     match calories with 
+     | [] -> 0
+     | _ -> (calories |> List.filter (fun (item) ->  item.Length = 0  ) |> List.length) + 1
+   
+let CaloriesPerElf (calories: string list) : int list =
+     [5000]
--- AdventOfCode/AdventOfCode.TestProject1/Tests.fs
+++ AdventOfCode/AdventOfCode.TestProject1/Tests.fs
@@ -4,9 +4,28 @@ open FunctionalDomain
 open DayOne
 open Xunit
 open FsUnit.Xunit
+open Xunit.Sdk
 
 [<Fact>]
-let ``count zero elf`` () = CountElves [] |> should equal 0 
+let ``count zero elf`` () = CountElves [] |> should equal 0
 
 [<Fact>]
-let ``count one elf`` () = CountElves ["2000"] |> should equal 1 
+let ``count one elf`` () = CountElves [ "2000" ] |> should equal 1
+
+
+let ``count one elf with two items`` () =
+    CountElves [ "2000"; "3000" ] |> should equal 1
+
+
+[<Fact>]
+let ``count two elves with one item`` () =
+    CountElves [ "2000"; ""; "3000" ] |> should equal 2
+
+
+[<Fact>]
+let ``count two elves the first one has two items and the second has one item`` () =
+    CountElves [ "2000"; "4000"; ""; "3000" ] |> should equal 2
+
+[<Fact>]
+let ``the first elf has 5000 calories`` () =
+    CaloriesPerElf [ "2000"; "3000" ] |> should equal [5000]
